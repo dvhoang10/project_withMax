68c64552b9fdea16950f37fffe0dd9d7e2851735
dvhoang10 <dvhlm10@gmail.com>
2023-08-04 11:45:21 +0700

    login effect

---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import * as AuthActions from './auth.actions';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable()
+export class AuthEffects {
+  authLogin = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(AuthActions.LOGIN_START),
+      switchMap((authData: AuthActions.LoginStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' +
+              environment.firebaseAPIKey,
+            {
+              email: authData.payload.email,
+              password: authData.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            map((resData) => {
+              const expirationDate = new Date(
+                new Date().getTime() + +resData.expiresIn * 1000
+              );
+              return new AuthActions.Login({
+                email: resData.email,
+                userId: resData.localId,
+                token: resData.idToken,
+                expirationDate: expirationDate,
+              });
+            }),
+            catchError((errorRes) => {
+              let errorMessage = 'An unknown error occurred!';
+              if (!errorRes.error || !errorRes.error.error) {
+                return of(new AuthActions.LoginFail(errorMessage));
+              }
+              switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                  errorMessage = 'This email exists already';
+                  break;
+                case 'EMAIL_NOT_FOUND':
+                  errorMessage = 'This email does not exist.';
+                  break;
+                case 'INVALID_PASSWORD':
+                  errorMessage = 'This password is not correct.';
+                  break;
+              }
+              return of(new AuthActions.LoginFail(errorMessage));
+            })
+          );
+      })
+    );
+  });
+
+  authSuccess = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.LOGIN),
+        tap(() => {
+          this.router.navigate(['/']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+}
